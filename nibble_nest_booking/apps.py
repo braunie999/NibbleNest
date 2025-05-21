e7a109ae389de558163993051490c35614d524cb
Kiara_Braun <kiarabraun5@gmail.com>
2025-05-21 10:42:58 +0000

    Initial Commit

---
+++ nibble_nest_booking/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NibbleNestBookingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "nibble_nest_booking"
---
+++ nibble_nest_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NibbleNestWebConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "nibble_nest_web"
