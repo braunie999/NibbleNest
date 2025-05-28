4d36cf6cdc715132685af047afee99ab91df95cb
Kiara_Braun <kiarabraun5@gmail.com>
2025-05-28 09:04:37 +0000

    Configuring previous commit to Nibble Nest project.

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "restaurant_booking.settings"
+        "DJANGO_SETTINGS_MODULE", "nibblenest.settings"
     )
     try:
         from django.core.management import execute_from_command_line
