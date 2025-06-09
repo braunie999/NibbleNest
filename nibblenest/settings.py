87a82d9fb952f786aa0a7739a304823622863a28
Kiara_Braun <kiarabraun5@gmail.com>
2025-06-09 10:24:04 +0000

    Importing some files from a directory

--- nibblenest/settings.py
+++ nibblenest/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +89,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
