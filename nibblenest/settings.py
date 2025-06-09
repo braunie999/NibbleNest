2a53737b146ac2b994115fd4a88ba48f644de099
braunie999 <kiarabraun5@gmail.com>
2025-06-09 15:49:06 +0200

    Modifying settings for heroku deployment

--- nibblenest/settings.py
+++ nibblenest/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-er52ta%n&94ris4fz@qdh6*u3)78$my&79q*_prff-%e4_i#al"
-)
+# SECRET_KEY = (
+#     "django-insecure-er52ta%n&94ris4fz@qdh6*u3)78$my&79q*_prff-%e4_i#al"
+# )
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
